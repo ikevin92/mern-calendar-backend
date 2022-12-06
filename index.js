42b288ab689bd63d9c56fe9c199459ff91603a58
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-12-05 22:02:12 -0500

    variables de entorno y carpeta publica

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 
 // crear servidor de express
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+
+// Directorio publico
+app.use(express.static('public'));
 
 
 // Rutas de mi app
-app.get('/', (req, res) => {
-
-  res.json({ ok: true, msg: 'Hola mundo' });
-});
+// app.get('/', (req, res) => {
+// });
 
 
 // Arrancar el servidor y escuchar peticiones
